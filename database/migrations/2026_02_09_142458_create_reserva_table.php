43612d43fb0145630959d6fcb1a89703017e71c5
Fortea Fatsini <464186.joan23@fje.edu>
2026-02-09 16:21:02 +0100

    Migraciones hechas (plantilla)

---
+++ database/migrations/2026_02_09_142458_create_reserva_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reserva', function (Blueprint $table) {
+            $table->id();
+
+            // -- Id user -- //
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+
+            // -- Id restaurante -- //
+            $table->unsignedBigInteger('id_restaurante');
+            $table->foreign('id_restaurante')->references('id')->on('restaurante')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reserva');
+    }
+};
---
+++ database/migrations/2026_02_09_142813_create_like_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('like', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('like');
+
+            // -- Id restaurante -- //
+            $table->unsignedBigInteger('id_restaurante');
+            $table->foreign('id_restaurante')->references('id')->on('restaurante')->onDelete('cascade');
+
+            // -- Id user -- //
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('like');
+    }
+};
