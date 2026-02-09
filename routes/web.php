4972f3f6f63e0f9642911991949f7a23bc42683f
Fortea Fatsini <464186.joan23@fje.edu>
2026-02-09 19:54:00 +0100

    Plantilla de la página create hecha.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\RestauranteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(RestauranteController::class)->group(function(){
+
+    Route::get('/crear-restaurante', 'index')->name('crear.restaurante');
+
 });
 
