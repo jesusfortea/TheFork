1ad79a88706384c8c00965e4572e7ce090f21e78
samuelmartin <samuelmartin991@gmail.com>
2026-02-09 16:56:34 +0100

    rutas login

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Vista del formulario
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+// Procesar el login
+Route::post('/login', [LoginController::class, 'login']);
+// Cerrar sesión
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
