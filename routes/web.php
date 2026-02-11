c01644ba56fc32bc37b6f06f0f25546662afd2fe
Fortea Fatsini <464186.joan23@fje.edu>
2026-02-11 17:35:15 +0100

    Correccion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(RestauranteController::class)->group(function(){
 
     Route::get('/crear-restaurante', 'index')->name('crear.restaurante');
-    Route::get('/restaurantes', 'show')->name('show.restaurante');
+    Route::get('/', 'show')->name('show.restaurante');
     Route::post('/solicitud-restaurante', 'solicitud')->name('enviar.solicitud');
 
 });
@@ -16,8 +16,8 @@ Route::controller(RestauranteController::class)->group(function(){
 // Vista del formulario
 Route::controller(LoginController::class)->group(function(){
 
-    Route::get('/', 'showLoginForm')->name('login');
-    Route::post('/', 'login')->name('login.post');
+    Route::get('/login', 'showLoginForm')->name('login');
+    Route::post('/login', 'login')->name('login.post');
     Route::post('/logout', 'logout')->name('logout');
 
 });
